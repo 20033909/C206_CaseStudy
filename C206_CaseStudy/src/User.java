778559a029097688efc20b17a4a4d9c7c6a4145b
22025566 <22025566@myrp.edu.sg>
2023-07-23 15:03:44 +0800

    Added the User.java file. Will work on the parent.java & vendor.java

---
+++ C206_CaseStudy/src/User.java
@@ -0,0 +1,94 @@
+
+public class User {
+	
+	private int userID;
+	private String username;
+	private String password;
+	private String email;
+	private String fullName;
+	private String phoneNo;
+	private String address;
+	private String role;
+	
+	
+	public User(int userID, String username, String password, String email, String fullName, String phoneNo, String address, String role) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullName = fullName;
+        this.phoneNo = phoneNo;
+        this.address = address;
+        this.role = role;
+    }
+	
+	// Getters
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    // Setters
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+	
+	
+	
+
+}
