a94ea4f0eee1d8fffdfab9080df657e5ef4f603c
sokhiennn <22013179@myrp.edu.sg>
2023-08-09 22:42:15 +0800

    sokhien updated 09/08/23

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,31 @@
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
 public class C206_CaseStudyTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    private List<School> schoolList;
+    private Admin admin;
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+        schoolList = new ArrayList<>();
+        admin = new Admin("admin123", "admin", "password", "admin@example.com", "Admin User", "12345678", "123 Main St", "Admin");
+    }
 
-	@Test
-	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
-	}
+    @After
+    public void tearDown() throws Exception {
+        schoolList = null;
+        admin = null;
+    }
 
+
+
+    @Test
+    public void c206_test() {
+        assertTrue("C206_CaseStudy_SampleTest", true);
+    }
 }
