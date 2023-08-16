5bc9b1baf525a257bab76dc3c457e935a75cf640
sokhiennn <131931503+sokhiennn@users.noreply.github.com>
2023-08-16 18:25:14 +0800

    Add files via upload

---
+++ C206_CaseStudy/src/src/Admin.java
@@ -0,0 +1,6 @@
+public class Admin extends User {
+    public Admin(String userID, String username, String password, String email, String fullName, String phoneNo, String address, String role) {
+        super(userID, username, password, email, fullName, phoneNo, address, role);
+    }
+}
+
