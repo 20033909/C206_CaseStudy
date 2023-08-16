3b017576a5150757667a33f8366aed2246173441
jocelyntan5 <131931552+jocelyntan5@users.noreply.github.com>
2023-08-16 21:10:40 +0800

    Jocelyn Updated
    Jocelyn Updated

---
+++ C206_CaseStudy/src/JocelynSourceCode/Admin.java
@@ -0,0 +1,7 @@
+package JocelynSourceCode;
+public class Admin extends User {
+    public Admin(String userID, String username, String password, String email, String fullName, String phoneNo, String address, String role) {
+        super(userID, username, password, email, fullName, phoneNo, address, role); 
+    }
+}
+
---
+++ C206_CaseStudy/src/JocelynSourceCode/School.java
@@ -0,0 +1,27 @@
+package JocelynSourceCode;
+
+public class School {
+    private String name;
+    private String distributionRange;
+
+    public School(String name, String distributionRange) {
+        this.name = name;
+        this.distributionRange = distributionRange;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDistributionRange() {
+        return distributionRange;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDistributionRange(String distributionRange) {
+        this.distributionRange = distributionRange;
+    }
+}
